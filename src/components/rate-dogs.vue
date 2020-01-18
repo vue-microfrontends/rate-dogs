f52266440ac8116075a0cf454a813f25bea317f8
Joel Denning <joeldenning@gmail.com>
2020-01-18 14:11:27 -0700

    Implementing dog ratings

--- src/components/rate-dogs.vue
+++ src/components/rate-dogs.vue
@@ -1,12 +1,78 @@
 <template>
   <div>
-    <page-header title="Rate some doggos"></page-header>
+    <page-header title="Rate doggos"></page-header>
+    <div class="content">
+      <div class="card">
+        <div v-if="currentDogUrl">
+          <img :src="currentDogUrl" alt="dog photo" />
+        </div>
+        <div class="actions">
+          <select v-model="rating" v-on:input="newRating()">
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+            <option value="13">13</option>
+            <option value="14">14</option>
+            <option value="15">15</option>
+          </select>
+          <button v-on:click="newDog()">Next dog</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { PageHeader } from "@vue-mf/styleguide";
 export default {
-  components: { PageHeader }
+  components: { PageHeader },
+  data: () => ({
+    currentDogUrl: null,
+    rating: "10"
+  }),
+  created() {
+    this.newDog();
+  },
+  methods: {
+    newDog() {
+      fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(json => {
+          this.currentDogUrl = json.message;
+          this.rating = "10";
+        });
+    },
+    newRating() {
+      const dogs = JSON.parse(localStorage.getItem("dogs")) || [];
+      dogs.push({ url: this.currentDogUrl, rating: Number(this.rating) });
+      this.newDog();
+    }
+  }
 };
 </script>
-<style scoped></style>
+<style scoped>
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+
+.card {
+  margin-top: 1.6rem;
+  padding: 3.2rem;
+  background-color: white;
+  border-radius: 0.5rem;
+  max-width: 75%;
+}
+
+.card img {
+  max-width: 100%;
+}
+
+.actions {
+  margin-top: 1.6rem;
+  display: flex;
+  justify-content: center;
+}
+</style>
