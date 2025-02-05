9bda94878e60a95426afe3ba2174d7e1334fcdb0
Joel Denning <joeldenning@gmail.com>
2025-02-04 23:13:28 -0600

    Switch to native modules, upgrade dependencies (#36)

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,9 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
+import { createWebHistory, createRouter } from 'vue-router'
 import RateDogs from "../components/rate-dogs.vue";
-
-Vue.use(VueRouter);
 
 const routes = [{ path: "/rate-doggos", component: RateDogs }];
 
-const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
+export default createRouter({
+  history: createWebHistory(),
   routes
-});
-
-export default router;
+})
--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,6 @@
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
-    devServer: {
-      headers: {
-        "Access-Control-Allow-Origin": "*"
-      },
-      disableHostCheck: true,
-      sockPort: 8503,
-      sockHost: "localhost",
-      https: true,
-      port: 8503
-    },
     externals: ["vue", "vue-router", /^@vue-mf\/.+/]
   },
   filenameHashing: false
